080aeeaebb845450574cb5be702e586f4464d01c
overflowed-stack <spaydermendima@gmail.com>
2023-03-04 23:02:30 -0800

    Setup service for parsing news with selenium and sending it to redis publisher with docker setup

---
+++ publish/server.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+import { Builder, By, Browser, until } from 'selenium-webdriver';
+
+const redisUrl = 'redis://redis:6379';
+const newsUrl = 'https://www.ukr.net/ua/news/main.html';
+const seleniumUrl = 'http://selenium-hub:4444/wd/hub';
+const $parseInterval = 5 * 1000;
+const $locateElemTimeout = 1000;
+
+const publisher = redis.createClient({url: redisUrl});
+
+await publisher.connect();
+
+const getNews = async () => {
+  const driver = await new Builder().forBrowser(Browser.CHROME).usingServer(seleniumUrl).build();
+
+  try {
+    await driver.get(newsUrl);
+
+    await driver.wait(until.elementLocated(By.css("a")), $locateElemTimeout);
+    const elements = await driver.findElements(By.css("a.im-tl_a"))
+  
+    const news = await Promise.all(elements.map(async el => await el.getText())); //remove async and await
+
+    await publisher.publish('article', JSON.stringify(news));
+  } finally {
+    await driver.quit();
+  }
+  setTimeout(getNews, $parseInterval);
+};
+
+getNews();
