f9b614f31bda9e0a2044cd8d23cf8c9f356271b4
overflowed-stack <spaydermendima@gmail.com>
2023-03-05 23:38:02 -0800

    Increase timeout between news parsing

--- publish/server.js
+++ publish/server.js
@@ -4,7 +4,7 @@ import { Builder, By, Browser, until } from 'selenium-webdriver';
 const redisUrl = 'redis://redis:6379';
 const newsUrl = 'https://www.ukr.net/ua/news/main.html';
 const seleniumUrl = 'http://selenium-hub:4444/wd/hub';
-const $parseInterval = 5 * 1000;
+const $parseInterval = 60 * 1000;
 const $locateElemTimeout = 1000;
 
 const publisher = redis.createClient({url: redisUrl});
@@ -20,7 +20,7 @@ const getNews = async () => {
     await driver.wait(until.elementLocated(By.css("a")), $locateElemTimeout);
     const elements = await driver.findElements(By.css("a.im-tl_a"))
   
-    const news = await Promise.all(elements.map(async el => await el.getText())); //remove async and await
+    const news = await Promise.all(elements.map(el => el.getText()));
 
     await publisher.publish('article', JSON.stringify(news));
   } finally {
